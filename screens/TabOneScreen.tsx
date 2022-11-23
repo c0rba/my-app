1540c2d16201bd670fdeca9f730409c9effe58a4
c0rba <56961722+c0rba@users.noreply.github.com>
2022-11-23 11:30:31 +0100

    Initial commit

---
+++ screens/TabOneScreen.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome!</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.nonTitle}>Välkommen till denna test app.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  nonTitle: {
+    fontSize: 13,
+    fontWeight: 'bold',
+  }
+});
